238ffcc88c322395ae48971ddf577c1ad4744f25
MartaOstash <martaostash.18@gmail.com>
2023-11-04 12:04:47 -0400

    I have added all features required for a car

---
+++ src/test/java/CarFeature1.java
@@ -0,0 +1,6 @@
+public class CarFeature1 {
+    String tire;
+    String tirecolor;
+    String tireWidth;
+    String tireModel;
+}
--- src/test/java/main.java
+++ src/test/java/main.java
@@ -5,5 +5,14 @@ public class main {
         audi.Hp="5000";
         audi.doors="2";
         audi.Model="A90";
+
+
+        CarFeature1 tire= new CarFeature1();
+        tire.tire="4";
+        tire.tirecolor="blue";
+        tire.tireModel="yokohama";
+        tire.tireWidth="18";
+        
     }
+
 }
